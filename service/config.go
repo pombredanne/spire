db7e153134c866cf7f6f9e60883df4450a889012
Haiko Schol <hs@haikoschol.com>
2017-07-19 11:44:28 +0200

    rename env variable to be consistent with the "device"/"control" naming

--- service/config.go
+++ service/config.go
@@ -2,9 +2,9 @@ package service
 
 // ConfigS defines all possible config params
 type ConfigS struct {
-	Environment    string `env:"ENV"  envDefault:"prod"`
-	DevicesBind    string `env:"DEVICES_BIND"  envDefault:":1883"`
-	ServicesBind   string `env:"SERVICES_BIND"  envDefault:":1884"`
+	Environment string `env:"ENV"  envDefault:"prod"`
+	DevicesBind string `env:"DEVICES_BIND"  envDefault:":1883"`
+	ControlBind string `env:"CONTROL_BIND"  envDefault:":1884"`
 }
 
 // Config is the global handle for accessing runtime configuration
